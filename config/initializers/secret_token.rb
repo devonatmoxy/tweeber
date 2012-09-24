a8713f1074daa0eab265a613b998ddcda0ca680e
Devon Moss <dvnmss@gmail.com>
2012-09-23 23:15:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweeber::Application.config.secret_token = 'dda5a1ae7ad068c7b29ec1c74e679fbdad1d517035e0affc452a78149fc080f0e1abd84c2d08416a3e3fb63fd447a1a50233030d0291082a3dea6608ee5b9bb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tweeber::Application.config.session_store :cookie_store, key: '_tweeber_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tweeber::Application.config.session_store :active_record_store
